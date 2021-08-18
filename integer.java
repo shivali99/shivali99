c4427c310b9c47f411c23a82b9bafffd0439e440
shivali99 <89121349+shivali99@users.noreply.github.com>
2021-08-18 12:11:27 +0530

    integer.java

---
+++ integer.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+ 
+class A
+{
+     
+     static void numbersWith3Divisors(int n)
+    {
+        boolean[] prime = new boolean[n+1];
+        Arrays.fill(prime, true);
+        prime[0] = prime[1] = false;
+  
+        for (int p = 2; p*p <= n; p++)
+        {
+             
+            
+            if (prime[p] == true)
+            {
+               
+                for (int i=p*2; i<=n; i += p)
+                    prime[i] = false;
+            }
+        }
+  
+       
+        System.out.println("Numbers with >3 divisors : ");
+        for (int i=0;  i*i <= n ; i++)
+            if (prime[i])
+                System.out.print(i*i + " ");
+    }
+      public static void main (String[] args)
+    {
+        int n = 10;
+        numbersWith >3 Divisors(n);
+    }
+}
+ /*OUTPUT/*
+ output:3,2
